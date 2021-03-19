11e41d2cf8677d8bbf9ce1a1fa61f04104966fc3
rudy0103 <qs2720@gmail.com>
2021-03-19 18:41:05 +0900

    update

---
+++ 6.Sort/6-1.py
@@ -0,0 +1,13 @@
+# p. 159 선택 정렬 소스코드
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+
+for i in range(len(array)):
+    min_index = i # rkwkd wkrdms dnjsthdml dlseprtm
+    for j in range(i+1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i] # swap
+
+
+print(array)
