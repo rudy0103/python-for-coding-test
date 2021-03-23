a6e631b6d3e662eff5520ed46ab9ce562d964cc7
rudy0103 <qs2720@gmail.com>
2021-03-23 15:42:48 +0900

    update

---
+++ 6.Sort/problem/3.py
@@ -0,0 +1,23 @@
+# p.182 실전문제 두 배열의 원소 교체
+
+n, k = map(int,input().split())
+
+
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+A.sort()
+B.sort(reverse=True)
+
+
+for i in range(k):
+    if A[i] < B[i]:
+        A[i],B[i] = B[i], A[i]
+    else:
+        break
+
+print(sum(A))
+
+
+
+
